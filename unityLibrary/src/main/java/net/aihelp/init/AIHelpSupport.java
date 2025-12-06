f64ac43332b60a680794df7ec71e10f525f52d6c
Daniil Pryima <swimmin2@gmail.com>
2025-12-06 20:35:50 +0200

    stub: Add AIHelpSupport.show
    Fixes [Menu] -> [Support] not working.

--- unityLibrary/src/main/java/net/aihelp/init/AIHelpSupport.java
+++ unityLibrary/src/main/java/net/aihelp/init/AIHelpSupport.java
@@ -1,10 +1,15 @@
 package net.aihelp.init;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
 import net.aihelp.config.UserConfig;
 import net.aihelp.ui.listener.OnAIHelpInitializedCallback;
+
+import jp.assasans.konofd.stub.MyApp;
 
 public class AIHelpSupport {
   public static void enableLogging(final boolean b) {
@@ -22,5 +27,27 @@ public class AIHelpSupport {
 
   public static void updateUserInfo(final UserConfig userConfig) {
     Log.d("AIHelpSupport", "updateUserInfo: " + userConfig);
+  }
+
+  // Invoked with [Menu] -> [Support].
+  // We can repurpose this for whatever we want.
+  public static boolean show(String entranceId) {
+    // 1 = help center
+    // 2 = faq
+    // 3 = single faq
+    // 4 = customer service
+    // other = error
+    Log.d("AIHelpSupport", "show: entranceId=" + entranceId);
+
+    // show toast for testing
+    Context context = MyApp.getAppContext();
+    Handler mainHandler = new Handler(Looper.getMainLooper());
+    mainHandler.post(() -> Toast.makeText(
+      context,
+      "AIHelpSupport.show() called with entranceId: " + entranceId,
+      Toast.LENGTH_SHORT
+    ).show());
+
+    return true;
   }
 }
